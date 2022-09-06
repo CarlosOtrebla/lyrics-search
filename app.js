89f9f93c27eee76838df32a9e8ce438cf39c4f2f
Carlos Alberto <carlosotrebla@gmail.com>
2022-09-06 01:45:32 -0300

    terceito commit

--- app.js
+++ app.js
@@ -5,12 +5,39 @@ const prevAndNextContainer = document.querySelector('#prev-and-next-container')
 
 const apiURL = `https://api.lyrics.ovh`
 
-const fetchSongs = term => {
-    fetch(`${apiURL}/suggest/${term}`)
-        .then(response => response.json())
-        .then(data => {
-            console.log(data)
-        })
+const getMoreSongs = async url => {
+    const response = await fetch(`https://cors-anywhere.herokuapp.com/${url}`)
+    const data = await response.json()
+
+    insertSongsIntoPage(data)
+}
+
+const insertSongsIntoPage = songsInfo => {
+    songsContainer.innerHTML = songsInfo.data.map(song=>`
+    <li class="song">
+        <span class="song-artist"><strong>${song.artist.name}</strong> - ${song.title} </span>
+        <button class="btn" data-artist="${song.artist.name}" data-song-title="${song.title}">Ver letra </button>
+    </li>
+    `).join('')
+
+    if (songsInfo.prev || songsInfo.next){
+        prevAndNextContainer.innerHTML = `
+        ${songsInfo.prev ? `<button class="btn" onClick="getMoreSongs('${songsInfo.prev}')">Anteriores</button>` : ''}
+        ${songsInfo.next ? `<button class="btn" onClick="getMoreSongs('${songsInfo.next}')">Próximas</button>` : ''}
+        `
+        return
+    }
+    
+    prevAndNextContainer.innerHTML = ''
+    
+}
+
+const fetchSongs = async term => {
+    const response = await fetch(`${apiURL}/suggest/${term}`)
+    const data = await response.json()
+
+    insertSongsIntoPage(data)
+        
 }
 
 form.addEventListener('submit', event => {
@@ -22,8 +49,29 @@ form.addEventListener('submit', event => {
         return
     }
 
-    fetchSongs(searchTerm)
+    fetchSongs(searchTerm)    
+})
 
+const fetchLyrics = async (artist, songTitle)=>{
+    const response = await fetch(`${apiURL}/v1/${artist}/${songTitle}`)
+    const data = await response.json()
 
-    
+    songsContainer.innerHTML =  `
+    <li class="lyrics-container">
+        <h2><strong>
+        ${songTitle}</strong> - ${artist}</h2>
+        <p class="lyrics">${data.lyrics}</p>
+    </li>`
+}
+
+songsContainer.addEventListener('click', event =>{
+
+    const clickedElement = event.target
+
+    if (clickedElement.tagName === 'BUTTON'){
+        const artist = clickedElement.getAttribute('data-artist')
+        const songTitle = clickedElement.getAttribute('data-song-title')
+
+        fetchLyrics(artist, songTitle)
+}
 })
